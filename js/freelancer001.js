0aacfa50037db6f8b66ef79c3785280eab8ced65
belindaquiroga <49857518+belindaquiroga@users.noreply.github.com>
2020-05-22 10:50:53 -0400

    Add files via upload

---
+++ js/freelancer001.js
@@ -0,0 +1,12 @@
+jQuer(document).ready(function($){
+  //Toggle nav menu on button click
+  var $root = $('html, body'),
+  menu_toggle = $('.menu_toggle');
+
+  menu_toggle.on('click', function(){
+    var $this = $(this);
+
+    $this.toggleClass('close')
+    $('.menu, .menu-nav, .menu-nav-item') .toggleClass('show');
+  });
+});
